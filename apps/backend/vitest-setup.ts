762b57c2e8b868e5fd204331cef8fe483a5d7b36
Jedai Saboteur <paradox.inversion@gmail.com>
2025-09-15 15:16:00 -0600

    chore: update vitest setup

--- apps/backend/vitest-setup.ts
+++ apps/backend/vitest-setup.ts
@@ -3,6 +3,8 @@ import { prepareDB } from "./test-helpers/prepareDB";
 import mongoose from "mongoose";
 import User from "./src/db/models/User";
 import Artist from "./src/db/models/Artist";
+import Track from "./src/db/models/Track";
+import Album from "./src/db/models/Album";
 
 beforeAll(async () => {
     await prepareDB();
@@ -10,5 +12,7 @@ beforeAll(async () => {
 afterAll(async () => {
     await User.deleteMany({});
     await Artist.deleteMany({});
+    await Track.deleteMany({});
+    await Album.deleteMany({});
     await mongoose.connection.dropDatabase();
 });
