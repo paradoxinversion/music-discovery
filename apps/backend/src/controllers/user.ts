da93c360799edbf0de2ee63b19eb4efbb8f6ec12
Jedai Saboteur <paradox.inversion@gmail.com>
2025-09-15 22:04:49 -0600

    chore: set up prettier

--- apps/backend/src/controllers/user.ts
+++ apps/backend/src/controllers/user.ts
@@ -2,4 +2,4 @@ import { Request, Response } from "express";
 import { IUser, IUserSignup } from "@common/types/src/types";
 const createUser = (req: Request, res: Response) => {    
     res.status(200).json({ status: "NOT IMPLEMENTED" });
-}
+};
