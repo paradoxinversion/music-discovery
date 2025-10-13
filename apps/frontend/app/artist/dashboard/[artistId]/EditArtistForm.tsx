24073f6c708725d5d4e575971cbe94c99a0fb1ba
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-13 14:08:18 -0600

    feat: add EditArtistForm component for editing artist details

---
+++ apps/frontend/app/artist/dashboard/[artistId]/EditArtistForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { EditableArtist } from "@common/types/src/types";
+import { useState } from "react";
+
+const socialPlatforms = [
+  "facebook",
+  "twitterX",
+  "instagram",
+  "tiktok",
+  "bluesky",
+];
+export default function EditArtistForm({
+  artistData,
+}: {
+  artistData?: EditableArtist;
+}) {
+  const [name, setName] = useState(artistData?.name || "");
+  const [genre, setGenre] = useState(artistData?.genre || "");
+  const [bio, setBio] = useState(artistData?.biography || "");
+  const [links, setLinks] = useState(artistData?.links || {});
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    if (socialPlatforms.includes(name)) {
+      setLinks((prevLinks) => ({ ...prevLinks, [name]: value }));
+    } else {
+      switch (name) {
+        case "artistName":
+          setName(value);
+          break;
+        case "genre":
+          setGenre(value);
+          break;
+        case "bio":
+          setBio(value);
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  return (
+    <form className="flex flex-col gap-4 mt-4">
+      <label>Artist Name</label>
+      <input
+        type="text"
+        name="artistName"
+        value={name}
+        onChange={handleInputChange}
+      />
+      <label>Genre</label>
+      <input
+        type="text"
+        name="genre"
+        value={genre}
+        onChange={handleInputChange}
+      />
+      <label>Bio</label>
+      <textarea name="bio" value={bio} onChange={handleInputChange}></textarea>
+      {socialPlatforms.map((platform) => (
+        <div key={platform} className="flex flex-col">
+          <label>{platform.charAt(0).toUpperCase() + platform.slice(1)}</label>
+          <input
+            type="text"
+            name={platform}
+            value={links[platform] || ""}
+            placeholder={`Enter your ${platform} link`}
+            onChange={handleInputChange}
+          />
+        </div>
+      ))}
+      <button type="submit">Save Changes</button>
+    </form>
+  );
+}
