d75b927d97db8d74f94c2cabc119622c35e1b1b9
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-13 15:40:13 -0600

    feat: enhance track page with conditional favorite button and login prompt

--- apps/frontend/app/track/[trackId]/page.tsx
+++ apps/frontend/app/track/[trackId]/page.tsx
@@ -85,16 +85,20 @@ export default function TrackPage({
             alt="Album Art"
             className="my-4 h-64 object-cover rounded-lg"
           />
-          <div onClick={likeTrack} className="cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className={`size-5 hover:fill-pink-700 ${trackFavorited ? "fill-pink-600" : "fill-gray-400"}`}
-            >
-              <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
-            </svg>
-          </div>
+          {user.loggedIn ? (
+            <div onClick={likeTrack} className="cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className={`size-5 hover:fill-pink-700 ${trackFavorited ? "fill-pink-600" : "fill-gray-400"}`}
+              >
+                <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
+              </svg>
+            </div>
+          ) : (
+            <p className="italic">Log in to add this track to your favorites</p>
+          )}
           <h1 className="text-2xl font-bold">{trackData.title}</h1>
           {trackData.artistId?.name && (
             <a href={`/artists/${trackData.artistId._id}`}>
@@ -102,7 +106,9 @@ export default function TrackPage({
             </a>
           )}
           {trackData.album && <a href="#">{trackData.album}</a>}
-          <p>Duration: {formatSecondsToMMSS(trackData.duration)}</p>
+          {trackData.duration && (
+            <p>Duration: {formatSecondsToMMSS(trackData.duration)}</p>
+          )}
         </div>
 
         <div id="track-external-links" className="w-1/3">
