dc14fad82411f106b9afc6603714e750494667a7
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-13 14:08:57 -0600

    feat: integrate Sidebar component into RootLayout for improved navigation

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
 import Header from "./login/Header";
+import Sidebar from "./Sidebar";
 
 export default function RootLayout({
   children,
@@ -13,7 +14,11 @@ export default function RootLayout({
         <body>
           <div className="@container mx-auto flex flex-col h-screen">
             <Header />
-            <div className="flex-grow">{children}</div>
+
+            <div className="flex flex-grow">
+              <Sidebar />
+              {children}
+            </div>
             <footer className="border-t border-gray-300 py-4">
               <p className="text-sm text-gray-500">
                 &copy; {new Date().getFullYear()} Jedai Saboteur. All rights
