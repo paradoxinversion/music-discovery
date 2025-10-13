75e8af69498b15d855d71e37a01627a3f8239209
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-13 14:56:02 -0600

    feat: add editArtistData function to handle artist data updates

---
+++ apps/frontend/actions/editArtistData.ts
@@ -0,0 +1,14 @@
+import { EditableArtist } from "@common/types/src/types";
+import axios from "axios";
+
+export default async function editArtistData(
+  artistId: string,
+  artistData: EditableArtist,
+) {
+  const response = await axios.put(
+    `${process.env.NEXT_PUBLIC_API_URL}/artists/${artistId}`,
+    artistData,
+    { withCredentials: true },
+  );
+  return response.data;
+}
