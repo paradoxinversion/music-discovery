eb00c349b99d369e97c54cb040e8cc7000b66d76
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-13 14:10:29 -0600

    feat: implement favorite artist functionality and enhance track details layout

--- apps/frontend/app/artists/[artistId]/page.tsx
+++ apps/frontend/app/artists/[artistId]/page.tsx
@@ -1,7 +1,12 @@
 "use client";
+
 import axios from "axios";
 import { useEffect, useState, use } from "react";
 import Link from "next/link";
+import setFavoriteArtist from "../../../actions/setFavoriteArtist";
+import { useAppSelector } from "../../../lib/hooks";
+import { useDispatch } from "react-redux";
+import { setFavoriteArtists } from "../../../lib/features/users/userSlice";
 
 export default function ArtistPage({
   params,
@@ -12,6 +17,19 @@ export default function ArtistPage({
   const [artistData, setArtistData] = useState(null);
   const [similarArtists, setSimilarArtists] = useState([]);
   const [otherArtists, setOtherArtists] = useState([]);
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const artistFavorited = user.favoriteArtists.includes(artistId);
+  const handleFavoriteClick = async () => {
+    try {
+      const response = await setFavoriteArtist(artistId, artistFavorited);
+      console.log("Favorite updated:", response);
+      dispatch(setFavoriteArtists(response.data));
+    } catch (error) {
+      console.error("Error updating favorite:", error);
+    }
+  };
+
   const fetchSimilarArtists = async (id: string) => {
     try {
       const response = await axios.get(
@@ -61,6 +79,16 @@ export default function ArtistPage({
     <div className="flex flex-grow py-2 px-4">
       <div id="artist-details" className="mr-8 w-1/2">
         <h1 className="text-2xl font-bold">{artistData.name}</h1>
+        <div onClick={handleFavoriteClick} className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className={`size-5 hover:fill-pink-700 ${artistFavorited ? "fill-pink-600" : "fill-gray-400"}`}
+          >
+            <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
+          </svg>
+        </div>
         <img
           src={"https://picsum.photos/1024/1024?random=1"}
           alt={artistData.name}
@@ -84,12 +112,12 @@ export default function ArtistPage({
           </a>
         </div>
       </div>
-      <div>
+      <div className="w-1/3">
         <div id="suggestions">
           <h2 className="text-xl font-semibold mt-4">You might also like:</h2>
           {similarArtists.length > 0 ? (
             <ul className="list-disc list-inside">
-              {similarArtists.map((artist: any) => (
+              {similarArtists.map((artist) => (
                 <li key={artist._id}>
                   <Link href={`/artists/${artist._id}`}>{artist.name}</Link>
                 </li>
@@ -103,7 +131,7 @@ export default function ArtistPage({
           <h2 className="text-xl font-semibold mt-4">Other artists:</h2>
           {otherArtists.length > 0 ? (
             <ul className="list-disc list-inside">
-              {otherArtists.map((artist: any) => (
+              {otherArtists.map((artist) => (
                 <li key={artist._id}>
                   <Link href={`/artists/${artist._id}`}>{artist.name}</Link>
                 </li>
--- apps/frontend/app/track/[trackId]/page.tsx
+++ apps/frontend/app/track/[trackId]/page.tsx
@@ -77,58 +77,68 @@ export default function TrackPage({
   }
 
   return (
-    <div className="flex flex-grow py-2 px-4">
-      <div id="track-details" className="mr-8 w-1/3">
-        <img
-          src="https://picsum.photos/512/512?random=1"
-          alt="Album Art"
-          className="my-4 h-64 object-cover rounded-lg"
-        />
-        <div onClick={likeTrack} className="cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className={`size-5 hover:fill-pink-700 ${trackFavorited ? "fill-pink-600" : "fill-gray-400"}`}
-          >
-            <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
-          </svg>
+    <div className="flex flex-grow h-full">
+      <div className="flex flex-grow p-4">
+        <div id="track-details" className="mr-8 w-1/3">
+          <img
+            src="https://picsum.photos/512/512?random=1"
+            alt="Album Art"
+            className="my-4 h-64 object-cover rounded-lg"
+          />
+          <div onClick={likeTrack} className="cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className={`size-5 hover:fill-pink-700 ${trackFavorited ? "fill-pink-600" : "fill-gray-400"}`}
+            >
+              <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
+            </svg>
+          </div>
+          <h1 className="text-2xl font-bold">{trackData.title}</h1>
+          {trackData.artistId?.name && (
+            <a href={`/artists/${trackData.artistId._id}`}>
+              {trackData.artistId.name}
+            </a>
+          )}
+          {trackData.album && <a href="#">{trackData.album}</a>}
+          <p>Duration: {formatSecondsToMMSS(trackData.duration)}</p>
         </div>
-        <h1 className="text-2xl font-bold">{trackData.title}</h1>
-        {trackData.artistId?.name && (
-          <a href={`/artists/${trackData.artistId._id}`}>
-            {trackData.artistId.name}
-          </a>
-        )}
-        {trackData.album && <a href="#">{trackData.album}</a>}
-        <p>Duration: {formatSecondsToMMSS(trackData.duration)}</p>
-      </div>
 
-      <div id="track-external-links" className="w-1/3">
-        <h1 className="text-2xl font-bold">Listen on:</h1>
-        <a href={`https://open.spotify.com/track/${trackId}`} target="_blank">
-          Spotify
-        </a>
-        <br />
-        <a href={`https://music.apple.com/us/album/${trackId}`} target="_blank">
-          Apple Music
-        </a>
-      </div>
-      <div id="track-suggestions">
-        <h2 className="text-xl font-semibold">You might also like:</h2>
-        {similarTracks.length > 0 ? (
-          <ul className="list-disc list-inside">
-            {similarTracks.map((track) => (
-              <li key={track._id}>
-                <a href={`/track/${track._id}`}>
-                  {track.title} by {track?.artistName}
+        <div id="track-external-links" className="w-1/3">
+          <h1 className="text-2xl font-bold">Listen on:</h1>
+          {trackData.links && Object.keys(trackData.links).length > 0 ? (
+            Object.keys(trackData.links).map((key, index) => (
+              <div key={index}>
+                <a
+                  href={trackData.links[key]}
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  {key.charAt(0).toUpperCase() + key.slice(1)}
                 </a>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No similar tracks found.</p>
-        )}
+              </div>
+            ))
+          ) : (
+            <p>No external links available.</p>
+          )}
+        </div>
+        <div id="track-suggestions" className="flex-grow">
+          <h2 className="text-xl font-semibold">You might also like:</h2>
+          {similarTracks.length > 0 ? (
+            <ul className="list-disc list-inside">
+              {similarTracks.map((track) => (
+                <li key={track._id}>
+                  <a href={`/track/${track._id}`}>
+                    {track.title} by {track?.artistName}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No similar tracks found.</p>
+          )}
+        </div>
       </div>
     </div>
   );
