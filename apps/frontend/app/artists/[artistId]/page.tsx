23793980fc98cbbd55eb2959f467666c076c3266
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-23 13:43:40 -0600

    refactor: use ImgContainer in artist page

--- apps/frontend/app/artists/[artistId]/page.tsx
+++ apps/frontend/app/artists/[artistId]/page.tsx
@@ -7,6 +7,7 @@ import setFavoriteArtist from "../../../actions/setFavoriteArtist";
 import { useAppSelector } from "../../../lib/hooks";
 import { useDispatch } from "react-redux";
 import { setFavoriteArtists } from "../../../lib/features/users/userSlice";
+import { ImgContainer } from "@mda/components";
 
 export default function ArtistPage({
   params,
@@ -89,10 +90,9 @@ export default function ArtistPage({
             <path d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
           </svg>
         </div>
-        <img
+        <ImgContainer
           src={"https://picsum.photos/1024/1024?random=1"}
           alt={artistData.name}
-          className="my-4 w-64 h-64 object-cover rounded"
         />
         <p>{artistData.biography}</p>
         <div id="artist-external-links" className="mt-2">
