13c398da2e8164d959ffca7a872d4d6f1ee76d97
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-28 14:23:34 -0600

    refactor: update login and signup forms, add axios instance for credentialed requests

--- apps/frontend/app/login/page.tsx
+++ apps/frontend/app/login/page.tsx
@@ -1,50 +1,35 @@
 "use client";
-import axios from "axios";
-import joi from "joi";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import checkAuthentication from "../../actions/checkAuthentication";
 import { useRouter } from "next/navigation";
 import { useAppDispatch } from "../../lib/hooks";
 import { setUser } from "../../lib/features/users/userSlice";
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
+import axiosInstance from "../../util/axiosInstance";
+import { ErrorText } from "@mda/components";
 
-const loginSchema = joi.object({
-  username: joi.string().min(3).max(30).required(),
-  password: joi.string().min(6).required(),
+interface LoginFormValues {
+  username: string;
+  password: string;
+}
+const loginSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Username must be at least 3 characters")
+    .max(30, "Username must be at most 30 characters")
+    .required("Username is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .max(50, "Password must be at most 50 characters")
+    .required("Password is required"),
 });
 
 export default function Page() {
   const router = useRouter();
   const dispatch = useAppDispatch();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "username") setUsername(value);
-    else if (name === "password") setPassword(value);
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { error, value } = loginSchema.validate({
-      username,
-      password,
-    });
-    if (error) {
-      throw new Error(error.message);
-    }
-    try {
-      const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/log-in`,
-        value,
-        {
-          withCredentials: true,
-        },
-      );
-      dispatch(setUser(res.data.user));
-      router.push("/discover");
-    } catch (error) {
-      console.error("Login error:", error.response.data);
-    }
+  const initialValues: LoginFormValues = {
+    username: "",
+    password: "",
   };
 
   useEffect(() => {
@@ -58,17 +43,54 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center min-h-screen justify-center py-2 w-full">
       <h1 className="text-3xl font-bold mb-4">Login</h1>
-      <form onChange={onChange} className="flex flex-col space-y-4 w-80">
-        <input name="username" type="text" placeholder="Username" />
-        <input name="password" type="password" placeholder="Password" />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-          onClick={handleSubmit}
-        >
-          Login
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginSchema}
+        onSubmit={async (values) => {
+          try {
+            const res = await axiosInstance.post("/auth/log-in", values);
+            dispatch(setUser(res.data.user));
+            router.push("/discover");
+          } catch (error) {
+            console.error("Login error:", error.response.data);
+          }
+        }}
+      >
+        {({ handleSubmit, errors, touched }) => (
+          <form
+            className="flex flex-col space-y-4 w-80 "
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit(e);
+            }}
+          >
+            <Field
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Username"
+            />
+            {errors.username && touched.username ? (
+              <ErrorText message={errors.username} />
+            ) : null}
+            <Field
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+            {errors.password && touched.password ? (
+              <ErrorText message={errors.password} />
+            ) : null}
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+            >
+              Login
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
