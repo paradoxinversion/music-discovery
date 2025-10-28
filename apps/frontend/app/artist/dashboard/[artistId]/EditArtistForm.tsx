6cb20ece7c6b60138ee9b2afa515c8171229bb5c
Jedai Saboteur <paradox.inversion@gmail.com>
2025-10-28 14:42:20 -0600

    refactor: use formik for form handling

--- apps/frontend/app/artist/dashboard/[artistId]/EditArtistForm.tsx
+++ apps/frontend/app/artist/dashboard/[artistId]/EditArtistForm.tsx
@@ -1,8 +1,22 @@
 "use client";
 import { EditableArtist } from "@common/types/src/types";
-import React, { useState } from "react";
+import React from "react";
 import editArtistData from "../../../../actions/editArtistData";
-import { Button } from "@mda/components";
+import { Button, ErrorText } from "@mda/components";
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
+
+interface EditArtistFormValues {
+  photo: File | string;
+  artistName: string;
+  genre: string;
+  bio: string;
+  facebook?: string;
+  twitterX?: string;
+  instagram?: string;
+  tiktok?: string;
+  bluesky?: string;
+}
 
 const socialPlatforms = [
   "facebook",
@@ -11,6 +25,19 @@ const socialPlatforms = [
   "tiktok",
   "bluesky",
 ];
+
+const editArtistSchema = Yup.object().shape({
+  artistName: Yup.string()
+    .min(2, "Artist name must be at least 2 characters")
+    .max(100, "Artist name must be at most 100 characters")
+    .required("Artist name is required"),
+  genre: Yup.string()
+    .max(50, "Genre must be at most 50 characters")
+    .required("Genre is required"),
+  bio: Yup.string().max(1500, "Bio must be at most 1500 characters"),
+  // Additional validations for social links can be added here
+});
+
 export default function EditArtistForm({
   artistData,
   artistId,
@@ -24,77 +51,92 @@ export default function EditArtistForm({
   >;
   setEditArtistDataAction: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const [name, setName] = useState(artistData?.name || "");
-  const [genre, setGenre] = useState(artistData?.genre || "");
-  const [bio, setBio] = useState(artistData?.biography || "");
-  const [links, setLinks] = useState(artistData?.links || {});
-
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const { name, value } = e.target;
-    if (socialPlatforms.includes(name)) {
-      setLinks((prevLinks) => ({ ...prevLinks, [name]: value }));
-    } else {
-      switch (name) {
-        case "artistName":
-          setName(value);
-          break;
-        case "genre":
-          setGenre(value);
-          break;
-        case "bio":
-          setBio(value);
-          break;
-        default:
-          break;
-      }
-    }
+  const initialValues: EditArtistFormValues = {
+    photo: "",
+    artistName: artistData?.name || "",
+    genre: artistData?.genre || "",
+    bio: artistData?.biography || "",
+    ...artistData?.links,
   };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const updateData = await editArtistData(artistId, {
-      name,
-      genre,
-      biography: bio,
-      links,
-    });
-    setArtistDataAction(updateData);
-  };
-
   return (
-    <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
-      <label>Artist Name</label>
-      <input
-        type="text"
-        name="artistName"
-        value={name}
-        onChange={handleInputChange}
-      />
-      <label>Genre</label>
-      <input
-        type="text"
-        name="genre"
-        value={genre}
-        onChange={handleInputChange}
-      />
-      <label>Bio</label>
-      <textarea name="bio" value={bio} onChange={handleInputChange}></textarea>
-      {socialPlatforms.map((platform) => (
-        <div key={platform} className="flex flex-col">
-          <label>{platform.charAt(0).toUpperCase() + platform.slice(1)}</label>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={editArtistSchema}
+      onSubmit={async (values) => {
+        const updateData = await editArtistData(artistId, {
+          name: values.artistName,
+          genre: values.genre,
+          biography: values.bio,
+          photo: values.photo,
+          links: socialPlatforms.reduce(
+            (acc, platform) => {
+              if (values[platform]) {
+                acc[platform] = values[platform];
+              }
+              return acc;
+            },
+            {} as { [key: string]: string },
+          ),
+        });
+        setArtistDataAction(updateData);
+      }}
+    >
+      {({ handleSubmit, setFieldValue, errors, touched }) => (
+        <form
+          className="flex flex-col gap-4 mt-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(e);
+          }}
+          encType="multipart/form-data"
+        >
+          <label>Artist Name</label>
+          <Field type="text" name="artistName" />
+          {errors.artistName && touched.artistName ? (
+            <ErrorText message={errors.artistName} />
+          ) : null}
+
+          <label>Photo</label>
           <input
-            type="text"
-            name={platform}
-            value={links[platform] || ""}
-            placeholder={`Enter your ${platform} link`}
-            onChange={handleInputChange}
+            type="file"
+            name="photo"
+            onChange={(event) =>
+              setFieldValue("photo", event.currentTarget.files[0])
+            }
           />
-        </div>
-      ))}
-      <Button label="Save Changes" type="submit" />
-      <Button label="Cancel" onClick={() => setEditArtistDataAction?.(false)} />
-    </form>
+
+          <label>Genre</label>
+          <Field type="text" name="genre" />
+          {errors.genre && touched.genre ? (
+            <ErrorText message={errors.genre} />
+          ) : null}
+
+          <label>Bio</label>
+          <Field as="textarea" name="bio" />
+          {errors.bio && touched.bio ? (
+            <ErrorText message={errors.bio} />
+          ) : null}
+
+          {socialPlatforms.map((platform) => (
+            <div key={platform} className="flex flex-col">
+              <label>
+                {platform.charAt(0).toUpperCase() + platform.slice(1)}
+              </label>
+              <Field
+                type="text"
+                name={platform}
+                placeholder={`Enter your ${platform} link`}
+              />
+            </div>
+          ))}
+
+          <Button label="Save Changes" type="submit" />
+          <Button
+            label="Cancel"
+            onClick={() => setEditArtistDataAction?.(false)}
+          />
+        </form>
+      )}
+    </Formik>
   );
 }
